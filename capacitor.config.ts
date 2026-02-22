f13b641f74c61411b4cedc54822c6f37eed8b761
Vladan Kudlac <vladan.kudlac@maptiler.com>
2026-02-22 23:45:23 +0100

    Migrate capacitor to v8

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,12 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.jakubhlavacek.gtfsrealtimemap',
   appName: 'FindMyBus',
-  webDir: 'build'
+  webDir: 'build',
+  plugins: {
+    SystemBars: {
+      insetsHandling: 'disable',
+    },
+  },
 };
 
 export default config;
