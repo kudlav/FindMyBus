4caeed31d70c1a054651345bae2ddf4d7deeaf48
Vladan Kudlac <vladan.kudlac@maptiler.com>
2026-08-19 15:50:00 +0200

    Set User-Agent when fetching tiles on mobile

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,6 +4,7 @@ const config: CapacitorConfig = {
   appId: 'cz.kudlav.gtfsrealtimemap',
   appName: 'FindMyBus',
   webDir: 'build',
+  overrideUserAgent: 'findmybus-app',
   plugins: {
     SystemBars: {
       insetsHandling: 'disable',
